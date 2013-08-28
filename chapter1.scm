888964ce30466886897949c746b3660e7d9052ba
Paul DeLong <pd72@nyu.edu>
2013-08-28 12:36:04 -0400

    added more of the Chatper 1 example code; not sure why Lagrangian-action doesn't work as expected (under MacOS; I'll have to try it on the Linux build to see if that makes any difference);

--- chapter1.scm
+++ chapter1.scm
@@ -1,7 +1,7 @@
 (define
-   ((L-free-particle-mass) local)
+   (  (L-free-particle mass) local)
    (let
-      ((v (velocity local)))
+      (  (v (velocity local)))
       (* 1/2 mass (dot-product v v))
    )
 )
@@ -15,3 +15,44 @@
 )
 
 (print-expression (q 't))
+
+(print-expression ((D q) 't))
+
+(print-expression
+   (  (compose
+         (L-free-particle 'm)
+         (Gamma q)
+      )
+      't
+   )
+)
+
+;(show-expression
+;   (  (compose
+;         (L-free-particle 'm)
+;         (Gamma q)
+;      )
+;      't
+;   )
+;)
+
+(define
+   (Lagrangian-action L q t1 t2)
+   (definite-integral (compose L (Gamma q)) t1 t2)
+)
+
+(define
+   (test-path t)
+   (up
+      (+ (* 4 t) 7)
+      (+ (* 3 t) 5)
+      (+ (* 2 t) 1)
+   )
+)
+
+(Lagrangian-action
+   (L-free-particle 3.0)
+   test-path
+   0.0
+   10.0
+)
