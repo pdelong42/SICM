c812dae9b6770f96d104490c9959f90209e52053
Paul DeLong <Paul.DeLong@gmail.com>
2015-05-28 09:15:25 -0400

    print-expression is superfluous for the purposes here; it also makes the output harder to read in non-interactive mode;

--- chapter8.scm
+++ chapter8.scm
@@ -17,39 +17,35 @@
 
 (* (cube 2) (sin 2))
 
-(print-expression ((compose cube sin) 'a))
+((compose cube sin) 'a)
 
-(print-expression
-   (  (-
-         (+
-            (square sin)
-            (square cos)  )
-         1  )
-      'a  )  )
+(  (- (+ (square sin)
+         (square cos)  )
+      1  )
+   'a  )
 
-(print-expression ((literal-function 'f) 'x))
+((literal-function 'f) 'x)
 
-(print-expression
-   (  (compose
-         (literal-function 'f)
-         (literal-function 'g)  )
-      'x  )  )
+(  (compose
+      (literal-function 'f)
+      (literal-function 'g)  )
+   'x  )
 
 (define g (literal-function 'g (-> (X Real Real) Real)))
 
-(print-expression (g 'x 'y))
+(g 'x 'y)
 
 (define v (up 'v^0 'v^1 'v^2))
 
-(print-expression v)
+v
 
 (define p (down 'p_0 'p_1 'p_2))
 
-(print-expression p)
+p
 
 (define s (up 't (up 'x 'y) (down 'p_x 'p_y)))
 
-(print-expression s)
+s
 
 ((component 0 1) (up (up 'a 'b) (up 'c 'd)))
 
@@ -57,21 +53,20 @@
 
 (ref (up (up 'a 'b) (up 'c 'd)) 0 1)
 
-(print-expression (* p v))
+(* p v)
 
 (define derivative-of-sine (D sin))
 
-(print-expression (derivative-of-sine 'x))
+(derivative-of-sine 'x)
 
-(print-expression
-   (  (  (*
-            (- D 1)
-            (+ D 1)
-         )
-         (literal-function 'f)  )
-      'x  )  )
+(((* 5 D) cos) 'x)
 
-(print-expression ((D g) 'x 'y))
+(  (  (* (- D I)
+         (+ D I)  )
+      (literal-function 'f)  )
+   'x  )
+
+((D g) 'x 'y)
 
 (define
    (h s)
@@ -79,13 +74,12 @@
       (ref s 0)
       (ref s 1)  )  )
 
-(print-expression (h (up 'x 'y)))
+(h (up 'x 'y))
 
-(print-expression ((D g) 'x 'y))
+((D g) 'x 'y)
 
-(print-expression
-   (  (D h)
-      (up 'x 'y)  )  )
+(  (D h)
+   (up 'x 'y)  )
 
 (define H
    (literal-function
@@ -97,9 +91,9 @@
             (DOWN Real Real)  )
          Real  )  )  )
 
-(print-expression (H s))
+(H s)
 
-(print-expression ((D H) s))
+((D H) s)
 
 (define
    (helix t)
@@ -110,7 +104,7 @@
 
 (define helix (up cos sin identity))
 
-(print-expression ((D helix) 't))
+((D helix) 't)
 
 (define
    (g x y)
@@ -119,7 +113,7 @@
       (cube   (- y x))
       (exp    (+ x y))  )  )
 
-(print-expression ((D g) 'x 'y))
+((D g) 'x 'y)
 
 (define
    (f x y)
