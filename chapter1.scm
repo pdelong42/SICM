32a599526bed68ce2c55d4da498b614b5826879c
Paul DeLong <Paul.DeLong@gmail.com>
2015-09-14 13:21:40 -0400

    pendulum on a pivot (pages 50-52);

--- chapter1.scm
+++ chapter1.scm
@@ -298,3 +298,32 @@
          (up (literal-function 'x_r)
              (literal-function 'y_r)))
       't  )  )
+
+(define
+   (  (T-pend m l g ys) local)
+   (let
+      (  (t            (time local))
+         (theta  (coordinate local))
+         (thetadot (velocity local))  )
+      (let*
+         (  (vys (D ys))
+            (vyst (vys t))  )
+         (* 1/2 m
+            (+ (square (* l thetadot))
+               (square vyst)
+               (* 2 l vyst thetadot (sin theta))  )  )  )  )  )
+
+(define
+   (  (V-pend m l g ys) local)
+   (let
+      (  (t (time local))
+         (theta (coordinate local))  )
+      (* m g (- (ys t) (* l (cos theta))))  )  )
+
+(define L-pend (- T-pend V-pend))
+
+(define demo1s
+   (  (  (Lagrange-equations (L-pend 'm 'l 'g (literal-function 'y_s)))
+         (literal-function 'theta)  )  't  )  )
+
+;(show-expression demo1s)
