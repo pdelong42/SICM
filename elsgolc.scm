a762b4980cc55f614ec9fcda7c2b6cbbcb209b0f
Paul DeLong <Paul.DeLong@gmail.com>
2016-09-14 09:30:40 -0400

    added a bunch of notes to the Elsgolc file;

--- elsgolc.scm
+++ elsgolc.scm
@@ -12,10 +12,21 @@
 ; Much of what you see below is unpolished, and I plan to fill-in some
 ; more exposition as I clean it up.
 
+; This example problem involves approximating the solution with a
+; trial function.  The author doesn't explain the motivation, but the
+; approach looks familiar.  He implies that the coefficients a_1 and
+; a_2 could continue indefinitely, but that he chose to stop at 2 (for
+; illustration purposes).  I'm guessing all the terms are mutually
+; orthogonal, but I haven't verified this myself.
+
+; y_2 is the candidate function:
+
 (define
    (y_2 x a_1 a_2)
    (* (square (- x 1))
       (+ a_1 (* a_2 x))  )  )
+
+; F is the function being minimized:
 
 (define
    (F x a b)
@@ -23,6 +34,10 @@
          (cube x)
          (square (D (D y_2)))  )
       (* b x (square y_2))  )  )
+
+; v_2 is the functional.  Below, I've integrated the integrand
+; manually, an integrand which I've found by evaluating F when
+; substituting y_2 into it.
 
 (define
    (v_2 x a b a_1 a_2)
@@ -48,9 +63,18 @@
       (* (expt x 3)  -2/3 b a_1 a_2)
       (* (expt x 2)  -1/2 b a_1 a_1)  )  )
 
+; Note: I integrated manually, because there isn't a general way to do
+; symbolic integration of closed-form expressions.  I'm 99% sure of
+; this, but it took me a while to remember this fact from basic calc
+; 1.  In other words, differentiation is always easier than
+; integration.  Hence integral tables.
+
 (define integrand1 ((F 'x 'a 'b) 'x 'a_1 'a_2))
 
 (define integrand2 (((partial 0) v_2) 'x 'a 'b 'a_1 'a_2))
+
+; Here, I evaluate the integral from x=0 to x=1 (I need a better name
+; for the result than v_3).
 
 (define
    (v_3 a b a_1 a_2)
